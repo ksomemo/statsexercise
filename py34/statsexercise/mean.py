3e217fa0c5a38e7def43e5cf109f25e33132ac82
ksomemo <kntsmy@hotmail.co.jp>
2015-09-13 19:22:00 +0900

    中央値を求める関数を実装した

--- py34/statsexercise/mean.py
+++ py34/statsexercise/mean.py
@@ -1,3 +1,13 @@
 def mean(xs):
     """算術平均"""
     return sum(xs) / len(xs)
+
+
+def median(xs):
+    """中央値"""
+    l = len(xs)
+    r = l % 2
+    q = l // 2
+    if r == 1:
+        return xs[q]
+    return mean([xs[q - 1], xs[q]])
--- py34/test/test_mean.py
+++ py34/test/test_mean.py
@@ -8,3 +8,11 @@ from statsexercise import mean
 ])
 def test_mean(xs, expected):
     assert mean.mean(xs) == expected
+
+
+@pytest.mark.parametrize('xs, expected', [
+    ([1, 2, 3, 5], 2.5),
+    ([1, 2, 5], 2)
+])
+def test_median(xs, expected):
+    assert mean.median(xs) == expected
